d8eb9508b59a00c22b997cd442ef202ce4e78fc6
Teerut26 <teerutsr@gmail.com>
2022-02-21 19:19:11 +0700

    init commit.

---
+++ src/App.svelte
@@ -0,0 +1,13 @@
+<script>
+    export let name;
+  </script>
+  
+  <main>
+    <div class="text-4xl">Hello {name}!</div>
+  </main>
+  
+  <style lang="postcss" global>
+    @tailwind base;
+    @tailwind components;
+    @tailwind utilities;
+  </style>
